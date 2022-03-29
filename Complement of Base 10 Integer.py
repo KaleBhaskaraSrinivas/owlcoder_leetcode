c57fcbe63365cc6a69a5f21bb081b4bbdf6d71b0
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-03-30 01:05:02 +0530

    Create Complement of Base 10 Integer.py

---
+++ Complement of Base 10 Integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        s=0
+        t=0
+        if n==0:
+            return 1
+        while n:
+            r=n%2
+            n=n//2
+            if r==0:
+                s=s+(2**t)
+            t=t+1
+        return s
