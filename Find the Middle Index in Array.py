ee075cd62ff2ee8676482370bab4d1f68641310b
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-03-31 23:13:42 +0530

    Update Find the Middle Index in Array.py

--- Find the Middle Index in Array.py
+++ Find the Middle Index in Array.py
@@ -9,5 +9,25 @@ class Solution:
                 return i
             left += x
         return -1
+    
+    
+    
+    class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        pr=[0 for i in range(len(nums))]
+        pr[0]=nums[0]
+        if nums==pr:
+            return 0
+        for i in range(1,len(nums)):
+            pr[i]=pr[i-1]+nums[i]
+        #return pr[-1]
+        if pr[0]==pr[-1]:
+            return 0
+        for i in range(len(pr)-1):
+            if pr[i]==pr[-1]-pr[i+1]:
+                    return i+1
+        else:
+            return -1
+    
         
             
