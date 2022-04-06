e06ad3b3d8659d725ea26e23ea2f49f244dc6475
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-04-06 18:48:53 +0530

    Update Find All Anagrams in a String.py

--- Find All Anagrams in a String.py
+++ Find All Anagrams in a String.py
@@ -17,3 +17,33 @@ class Solution:
                 res.append(i)
         return res
                 
+approach-2:
+    
+ class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        def is_Anagram(frqp,frqw):
+            for i in range(26):
+                if frqp[i]!=frqw[i]:
+                    return False
+            return True
+        frqp=[0]* 26
+        frqw=[0]* 26
+        n=len(p)
+        m=len(s)
+        if(n>m):
+            return []
+        for i in range(n):
+            frqp[ord(p[i])-ord('a')]+=1
+            frqw[ord(s[i])-ord('a')]+=1
+        left=0
+        right=n
+        l=[]
+        while(right<=m):
+            if is_Anagram(frqp,frqw):
+                l.append(left)
+            frqw[ord(s[left])-ord('a')]-=1
+            if right<m:
+                frqw[ord(s[right])-ord('a')]+=1
+            left+=1
+            right+=1
+        return l   
