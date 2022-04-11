f3938bb26664e6f3c4e3b46d9b6cfd0ebcc49899
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-04-11 18:26:56 +0530

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -18,3 +18,27 @@ public:
        return vec;
     }
 };
+
+
+class Solution {
+public:
+    void rec(int ind,vector<vector<int>> &ans,vector<int>nums,vector<int>&temp)
+    {
+        if(ind==nums.size())
+        {
+            ans.push_back(temp);
+            return ;
+        }
+        temp.push_back(nums[ind]);
+        rec(ind+1,ans,nums,temp);
+        temp.pop_back();
+        rec(ind+1,ans,nums,temp);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+       vector<vector<int>>ans;
+        vector<int>temp;
+        rec(0,ans,nums,temp);
+        return ans;
+    }
+};
