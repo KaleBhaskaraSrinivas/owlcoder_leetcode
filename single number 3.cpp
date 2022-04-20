d92dee2ab4c5800711f1898ce582443b169c625a
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-04-20 18:21:37 +0530

    Update single number 3.cpp

--- single number 3.cpp
+++ single number 3.cpp
@@ -15,3 +15,44 @@ public:
         
     }
 };
+
+
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(auto it:nums){
+            ans^=it;
+        }
+        int p=1;
+        int i=1;
+        while(1){
+            if(ans & i ){
+                break;
+            }
+            p++;
+            i=i<<1;
+        }
+        int res1=0;
+        int res2=0;
+        for(auto x:nums){
+            if(x & (1<<(p-1))){
+                res1^=x;
+            }
+            else{
+                res2^=x;
+            }
+        }
+        return {res1,res2};
+    }
+};
+
+
+
+
+
+
+
+
+
