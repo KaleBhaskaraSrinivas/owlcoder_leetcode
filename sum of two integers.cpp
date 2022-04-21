b9145aed7c30388d79255f93c52e6a9396aaaad6
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-04-21 18:33:59 +0530

    Update sum of two integers.cpp

--- sum of two integers.cpp
+++ sum of two integers.cpp
@@ -13,3 +13,21 @@ public:
     return sum;
     }
 };
+
+
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+    unsigned int c=a&b;
+    int sum=a^b;
+    while(c!=0)
+    {
+        a=sum;
+        b=c<<1;
+        sum=a^b;
+        c=a & b;
+    }
+    return sum;
+    }
+};
