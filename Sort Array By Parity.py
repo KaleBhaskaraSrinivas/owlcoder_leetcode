bde14b05f928a965b218cd6a9dc379251c27b8cf
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-05-02 17:14:49 +0530

    Update Sort Array By Parity.py

--- Sort Array By Parity.py
+++ Sort Array By Parity.py
@@ -8,3 +8,14 @@ class Solution:
                 arr.appendleft(i)
         return arr
         
+        
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        res=[]
+        for i in nums:
+            if i%2==0:
+                res.insert(0,i)
+            else:
+                res.append(i)
+        return res
+        
