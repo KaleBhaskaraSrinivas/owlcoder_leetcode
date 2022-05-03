1105971a2a55fdb96c6fe8ecf035bb28d32b8fd0
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-05-03 18:22:45 +0530

    Create same tree.cpp

---
+++ same tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == NULL and q == NULL)
+            return true;
+        
+        if((p == NULL and q != NULL) or (p != NULL and q == NULL) or (p->val != q->val))
+            return false;
+        
+        return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+    }
+};
