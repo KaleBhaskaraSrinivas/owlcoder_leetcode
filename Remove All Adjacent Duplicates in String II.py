dd057a66ebf0d036a14f8c6d6c46bb06409e9050
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-05-06 13:12:31 +0530

    Create Remove All Adjacent Duplicates in String II.py

---
+++ Remove All Adjacent Duplicates in String II.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        for i in range(len(s) - k, -1, -1):
+            if s[i] * k == s[i : i + k]: 
+                s = s[:i] + s[i + k :] 
+        return s
+        
